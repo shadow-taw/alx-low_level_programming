8c380d724a4f74e1fed079ce685b81fb893bf3a7
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-03-24 15:17:56 -0700

    respecting betty stylle

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,21 +1,16 @@
 #include "main.h"
 
 /**
- * * main - tests function that prints if integer is positive or negative
- * * Return: 0
- * */
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
 int main(void)
 {
 	int i;
 
-	i =0;
-	test=positive_or_negative(i);
-	if(test=="0 is zero"){
-		printf("the function is working well");
-	}else{
-		printf("the function is't working");
-	}
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }
