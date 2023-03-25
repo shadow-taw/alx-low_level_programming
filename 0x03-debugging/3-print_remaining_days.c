d3695ed520694dfe91481ce74c3f4e950fab5f66
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-03-24 19:57:22 -0700

    updating

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,25 +12,25 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-		int days_in_year;
+	int days_in_year;
 
-		if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
-		{
-			/* leap year */
-			days_in_year = 366;
-			if (month > 2 && day >= 60)
-				day++;
-		}
-		else
-		{
-		/* not a leap year */
-		days_in_year = 365;
-			if (month == 2 && day == 60)
-															{
-																printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-																return;
-												
-															}
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	{
+		/* leap year */
+		days_in_year = 366;
+		if (month > 2 && day >= 60)
+			day++;
+	}
+	else
+	{
+	/* not a leap year */
+	days_in_year = 365;
+		if (month == 2 && day == 60)
+														{
+															printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+															return;
+											
+														}
 	}
 
 	printf("Day of the year: %d\n", day);
