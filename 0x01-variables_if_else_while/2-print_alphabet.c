7c96774f6b4e3b7f0251bd7f9545f325570c942e
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-03-27 15:24:18 -0700

    updating

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main() {
+	int i;
+	for(i = 97; i <= 122; i++) {
+	        putchar(i);
+	}
+	putchar('\n');
+	return 0;
+}
