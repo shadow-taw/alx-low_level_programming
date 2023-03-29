476a98547969c41e52b4e8c1db6a2dca7e0537bb
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-03-29 14:19:09 -0700

    adding betty style for 2 time

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main
+ * main - print size of data type
  * compiled and run on.
  *
  * Return: Always 0 (Success)
@@ -12,6 +12,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
- 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
