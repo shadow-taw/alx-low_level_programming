1c40e7b0759ff020e7321f63831d79e628567ab1
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-03-29 15:33:20 -0700

    creating 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - printing alphabet
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 0 ; j <= 10 ; j++)
+	{
+		for (i = 97 ; i <= 122 ; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
