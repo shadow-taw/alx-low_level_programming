454ae956d2dc61236f691d72d5c2ac6d09ca0b2d
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-03-28 19:11:05 -0700

    adding betty style

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,7 +4,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main() {
+int main(void)
+{
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
@@ -16,6 +17,7 @@ int main() {
 			break;
 		}
 		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 
