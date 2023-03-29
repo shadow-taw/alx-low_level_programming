983a422dea3ae27573da27e6bb0810d7c201a708
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-03-29 14:57:37 -0700

    make my file exactly like main.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,17 +6,6 @@
  * print_alphabet - printing alphabet
  * Return: Always 0.
  */
-void print_alphabet(void)
-{
-	int i;
-
-	for (i = 97 ; i <= 122 ; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
-	return;
-}
 int main(void)
 {
 	print_alphabet();
