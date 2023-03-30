6fe7e8e42c1c10b5b0d5bb04b04637b7ea493023
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-03-30 16:30:26 -0700

    creating 2-mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - add 2 numbers
+ * @a: first number
+ * @b: secend number
+ * Return: the multiplipli of two numbers
+ */
+int mul(int a, int b)
+{
+	int x;
+
+	x = i + j;
+	return (x);
+}
