011df121c4d350310300daeb275ae8908c917401
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-03-29 17:07:19 -0700

    fixing errores

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		_putchar(1);
+		putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
-		_putchar(0);
+		putchar('0');
+		return (0);
 	}
 	else
 	{
-		_putchar('-');
-		_putchar('/');
+		putchar('-');
+		return (/);
 	}
 }
