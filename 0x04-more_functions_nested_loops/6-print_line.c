4fc6a2ba2e15e7b1ae31b27cfbc3a5e0e94647df
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-03-30 21:00:19 -0700

    creating 6-print_line.c file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - print a line in terminal
+ * @n: number of the lhent of the line
+ * Return: a line
+ */
+int print_line(int n)
+{
+	int i;
+
+	for (; i < n ; i++)
+	{
+		_putchar('_');
+	}
+	printf("\n");
+}
