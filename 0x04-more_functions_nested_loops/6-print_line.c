70cc9ab7d97d073a743c92d4410e9e3e7b185d94
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-04-01 14:29:48 -0700

    updating code

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,9 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
+	for (i = 0; i <= n ; i++)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (; i <= n ; i++)
-		{
-			_putchar('_');
-		}
+		_putchar('_');
 	}
 	_putchar('\n');
 }
