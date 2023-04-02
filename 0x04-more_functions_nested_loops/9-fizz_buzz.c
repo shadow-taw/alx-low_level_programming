8093620e6c54b2249151d67882cb0a2ea1e4f9e4
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-04-01 17:31:00 -0700

    updating

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,27 +9,20 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 5 == 0 && i % 3 == 0)
+		if (i % 3 == 0)
 		{
-			printf("FizzBuzz");
-			printf(" ");
+		printf("Fizz");
 		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz");
-			printf(" ");
-		}
-		else if (i % 5 == 0)
+		if (i % 5 == 0)
 		{
 			printf("Buzz");
-			printf(" ");
 		}
-		else
+		if (i % 5 != 0 && i % 3 != 0)
 		{
 			printf("%d", i);
+		}
 			printf(" ");
 		}
-	}
 	printf("\n");
 	return (0);
 }
