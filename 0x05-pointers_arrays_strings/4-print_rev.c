b79cae54805ba76202563b31bf9deae336418433
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-04-03 14:22:36 -0700

    creating 4-print_rev.c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * print_rev - reverse any kind of string
+ * @s: the string
+ */
+void print_rev(char *s)
+{
+	    int i, len = strlen(s);
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
