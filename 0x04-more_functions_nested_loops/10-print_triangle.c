ec867e71d05d894e8f260ee6bbe5465669ee3a5a
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-04-02 20:56:22 -0700

    creating 10-print_triangle.c file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_triangle - print a triangle
+ * @size: the size of the tringle
+ * Return: a tringle
+ */
+void print_triangle(int size)
+{
+	int i, j, y;
+
+	for (i = size; i > 0; i--)
+	{
+	for (j = 1; j < i; j++)
+	{
+		_putchar(' ');
+	}
+	for (y = size; y >= i; y--)
+	{
+		_putchar('#');
+	}
+		_putchar('\n');
+	}
+}
