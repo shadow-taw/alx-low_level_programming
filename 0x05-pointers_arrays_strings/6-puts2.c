8dd50d03227e8d0b906ac91860fd9f02aca9a75d
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-04-03 16:14:18 -0700

    creating 6-puts2.c file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+/**
+ * puts2 -  prints every other character of a string,
+ * @str: the string
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i += 2;
+	}
+	putchar('\n');
+}
