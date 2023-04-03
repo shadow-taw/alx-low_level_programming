630202c38aafbd6cc89b511aeeb4c68e521c66f2
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-04-03 03:51:23 -0700

    creating 2-strlen.c file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _strlen - cont the lenght of a name
+ * @s: the name value
+ * Return: a variable, the lenght of the name
+ */
+int _strlen(char *s)
+{
+	int i, a;
+
+	for (i = 0; i <= strlen(s); i++)
+	{
+		a = i;
+	}
+	return (a);
+}
