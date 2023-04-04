f691f856c5cca7838f1cc7ca673b2b8861ee751c
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-04-04 15:58:17 -0700

    creating 8-print_array.c file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_array - print the values of an arrey
+ * @a: the arrye
+ * @n: number of values in arrye
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		printf("%s, ", a[i]);
+	}
+	printf("\n");
+}
