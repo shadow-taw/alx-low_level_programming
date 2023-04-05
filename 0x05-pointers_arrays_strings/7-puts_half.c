4066dc98c3bab90ee3504febb35396763f095bbf
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-04-05 14:21:27 -0700

    updating

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,14 +10,14 @@ void puts_half(char *str)
 
 	if (l % 2 == 1)
 	{
-		for (i = (l - 1) / 2; i  < l; i++)
+		for (i = (l + 1) / 2; i  < l; i++)
 		{
 			putchar(str[i]);
 		}
 	}
 	else
 	{
-		for (i = (l / 2) + 1; i  < l; i++)
+		for (i = l / 2; i  < l; i++)
 		{
 			putchar(str[i]);
 		}
