25e50330a51cb179427d5fe7115cbd4bb2e65d89
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-04-12 08:02:52 -0700

    updating code

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
- * _strdup - print copy of the string given as a parameter
+ * _strdup - return a copy of the string given as a parameter
  * @str: the string given
  * Return: NULL if str is NULL, a pointer otherways
  */
 char *_strdup(char *str)
 {
-	str = malloc(sizeof(char));
+	int l, i;
 
-	if (str == 0)
+	l = strlen(str);
+	char *s = malloc(sizeof(l));
+
+	if (str == 0 || s == 0)
 	{
 		return (0);
 	}
 	else
 	{
-		return (str);
-		free(str);
+		l = strlen(str);
+		for (i = 0; i < l; i++)
+		{
+			s[i] = str[i];
+		}
+		return (s);
 	}
 }
