4aa6e90b1b3d657fc2a050300aa404e483ddf3ad
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-04-14 15:27:41 -0700

    creating README.md, main.h and 0-malloc_checked.c files

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - reserve memory
+ * @b: the size of memory needed
+ * Return: a pointer, or 98 if erorre
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *i;
+
+	i = malloc(b);
+	if (b != 0)
+	{
+		return (i);
+	}
+	else
+	{
+		_putchar(9 + '0');
+		_putchar(8 + '0');
+		_putchar('\n');
+	}
+}
