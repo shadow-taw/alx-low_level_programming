0a7f4aa3cdf309a2eefb29800edea7c30bc39f72
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-04-15 16:57:42 -0700

    creating 1-args.c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - test
+ * @argc: argc
+ * @argv: argv
+ * return: nuber of argument
+ */
+void main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+}
