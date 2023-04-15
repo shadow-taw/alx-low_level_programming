17203f0e7666d5c2b11a9739ccd8616bf422a68c
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-04-15 16:37:36 -0700

    creating README.md, main.h 0-whatsmyname.c files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - test code
+ * @argc: the argc
+ * @argv: the argv
+ *
+ * Return: print file name
+ */
+void main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+}
