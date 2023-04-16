cd4e96f9276aa6660d047ae2e8f27f67de3c182e
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-04-15 17:29:15 -0700

    creating 3-mul.c file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - test
  * @argc: argc
  * @argv: argv
- * Return: multiplication of to numbers
+ * Return: multiplication of tow numbers
  */
 int main(int argc, char **argv)
 {
 	(void)argc;
-	printf("%d\n", atoi(argv[1]) - atoi(argv[2]));
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
