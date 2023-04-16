5ea1fd56537a9c0801eaa93268b3a49f56cf4858
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-04-15 19:21:50 -0700

    updating

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 	(void)argc;
 	if (argc < 3 || argc > 3)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
