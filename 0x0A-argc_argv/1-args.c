7fe93f167bc1440c1d820d9cc5426f3bd6cf3584
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-04-15 17:05:51 -0700

    updating

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - test
  * @argc: argc
- * @argv: argv
  * Return: number of argument
  */
 int main(int argc, char **argv)
 {
 	(void)argv;
 	printf("%d\n", argc - 1);
+	return (0);
 }
