b8fbd4b5109fd5504113ea4bf2458cfa2f40374b
shadow-taw <62027129+shadow-taw@users.noreply.github.com>
2023-04-20 00:55:17 +0200

    Deleting 0x09-static_libraries

--- 0x09-static_libraries/3-puts.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/**
- * _puts - print a string followed by a new line, to stdout
- * @str: the string value
- */
-void _puts(char *str)
-{
-	puts(str);
-}
--- 0x09-static_libraries/README.md
+++
@@ -1,1 +0,0 @@
-Directory about static libraries
