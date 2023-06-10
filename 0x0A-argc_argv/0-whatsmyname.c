8540700fc2eee6b65f569f583d0c8989bdc6fa23
taoufik oubrahim <tawfikoubrahim@gmail.com>
2023-06-09 18:34:24 -0700

    creating 0-whatsmyname.c, main.h and README.md files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print it's name
+ * @argc: number of line in command line
+ * @argv: list of command line entred
+ * Return: return name of programme
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
